42f44fae5d5db63e4ca11145afabe63ca8df008c
Devan Irvin <devanirvin@yahoo.com>
2019-10-08 19:39:45 -0400

    accounts manager

---
+++ Account.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+    social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema)
